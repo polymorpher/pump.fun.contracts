0b3ed081b1c070e1fde57385f0713a69374a4eee
potvik <yuriy@harmony.one>
2024-12-16 20:25:14 +0300

    fixed e2e tests

--- test/E2E.t.sol
+++ test/E2E.t.sol
@@ -4,6 +4,7 @@ import "forge-std/Test.sol";
 import "@contracts/TokenFactory.sol";
 import "@contracts/BancorBondingCurve.sol";
 import "@contracts/Token.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
 contract E2ETest is Test {
     TokenFactory internal tf;
@@ -25,6 +26,16 @@ contract E2ETest is Test {
     address Dave = address(0x1238);
     address Server = address(0x1239);
 
+    event BurnTokenAndMintWinner(
+        address indexed sender,
+        address indexed token,
+        address indexed winnerToken,
+        uint256 burnedAmount,
+        uint256 receivedETH,
+        uint256 mintedAmount,
+        uint256 timestamp
+    );
+
     function setUp() public {
         vm.deal(Alice, 100 ether);
         vm.deal(Bob, 100 ether);
@@ -33,6 +44,7 @@ contract E2ETest is Test {
         Token tref = new Token();
         vm.startPrank(Owner);
         vm.deal(Owner, 1000 ether);
+
         bc = new BancorBondingCurve(SLOPE_SCALED, WEIGHT_SCALED);
         tf = new TokenFactory(address(tref), UNISWAP_V3_FACTORY, UNISWAP_V3_NPM, address(bc), WETH, FEE_PERCENT);
         vm.stopPrank();
@@ -51,13 +63,21 @@ contract E2ETest is Test {
         tc = tf.createToken("c", "C", "https://c.local");
         tf.buy{value: 1 ether}(address(tc));
         vm.stopPrank();
-
     }
 
     function test_publishToUniswap() public {
         vm.startPrank(Dave);
+        uint256 taBalanceBefore = ta.balanceOf(Dave);
+        uint256 calculatedReceiveAmountBefore = tf._buyReceivedAmount(address(ta), 0.1 ether);
+        // buy
         tf.buy{value: 0.1 ether}(address(ta));
-        // TODO: check price, token balance, ... after each buy
+
+        uint256 taBalanceAfter = ta.balanceOf(Dave);
+        assertApproxEqRel(calculatedReceiveAmountBefore, taBalanceAfter - taBalanceBefore, 0.0001 ether, "received amount wrong");
+
+        uint256 calculatedReceiveAmountAfter = tf._buyReceivedAmount(address(ta), 0.1 ether);
+        assertTrue(calculatedReceiveAmountAfter < calculatedReceiveAmountBefore, "the price has not increased");
+
         tf.buy{value: 0.1 ether}(address(ta));
         tf.buy{value: 0.05 ether}(address(tb));
         tf.buy{value: 0.02 ether}(address(tc));
@@ -65,38 +85,79 @@ contract E2ETest is Test {
 
         vm.startPrank(Owner);
         tf.startNewCompetition();
-        // TODO: check fee, test withdraw fee
+        // check CompetitionId increased
+        uint256 currentCompetitionId = tf.currentCompetitionId();
+        assertEq(currentCompetitionId, 2, "currentCompetitionId not 2");
         vm.stopPrank();
 
-        vm.startPrank(Server);
-        vm.expectRevert();
-        tf.publishToUniswap(address(tb));
-        vm.expectRevert();
-        tf.publishToUniswap(address(tc));
-        // TODO: check event emission, token minting amount...
-        tf.publishToUniswap(address(ta));
-        vm.stopPrank();
-        // TODO: check uniswap pool liquidity and price
-
-        vm.startPrank(Bob);
-        tf.burnTokenAndMintWinner(address(tb));
-        // TODO: check event emission, token minting amount...
-        vm.stopPrank();
-
-        vm.startPrank(Charlie);
-        tf.burnTokenAndMintWinner(address(tc));
-        // TODO: check event emission, token minting amount...
+        // user can't buy more token after startNewCompetition
+        vm.startPrank(Dave);
+        vm.expectRevert("The competition for this token has already ended");
+        tf.buy{value: 0.1 ether}(address(ta));
+        vm.expectRevert("The competition for this token has already ended");
+        tf.buy{value: 0.1 ether}(address(tb));
+        vm.expectRevert("The competition for this token has already ended");
+        tf.buy{value: 0.1 ether}(address(tc));
         vm.stopPrank();
 
         vm.startPrank(Dave);
+        vm.expectRevert("Token address is winner");
+        tf.burnTokenAndMintWinner(address(ta));
+
+        taBalanceBefore = ta.balanceOf(address(Dave));
+
+        uint256 burnedAmount = tb.balanceOf(address(Dave));
+        uint256 receivedETH = tf._sellReceivedAmount(address(tb), burnedAmount);
+        uint256 mintedAmount = tf._buyReceivedAmount(address(ta), receivedETH);
+
+        vm.expectEmit(true, true, true, true, address(tf));
+        emit BurnTokenAndMintWinner(
+            address(Dave), // sender
+            address(tb), // token
+            address(ta), // winnerToken
+            burnedAmount,
+            receivedETH,
+            mintedAmount,
+            block.timestamp
+        );
         tf.burnTokenAndMintWinner(address(tb));
-        // TODO: check event emission, token minting amount...
-        tf.burnTokenAndMintWinner(address(tc));
-        // TODO: check event emission, token minting amount...
+
+        taBalanceAfter = ta.balanceOf(address(Dave));
+        assertEq(mintedAmount, taBalanceAfter - taBalanceBefore, "taBalance incorrect");
         vm.stopPrank();
 
-        // TODO: have alice to trade some tokens on uniswap
+        vm.startPrank(Server);
+        vm.expectRevert('Token address not winner');
+        tf.publishToUniswap(address(tb));
 
-        // TODO: have bob / charlie / dave to trade token ta on uniswap
+        vm.expectRevert('Token address not winner');
+        tf.publishToUniswap(address(tc));
+
+        address tokenPool = tf.tokensPools(address(ta));
+        assertEq(tokenPool, address(0x0), 'Token pool address not 0x0');
+
+        // calculate liquidity
+        uint256 currentCollateral = tf.collateralById(currentCompetitionId - 1, address(ta));
+        uint256 totalCollateralFromAllTokens = tf.getCollateralByCompetitionId(currentCompetitionId - 1);
+        uint256 numTokensPerEther = bc.computeMintingAmountFromPrice(currentCollateral, ta.totalSupply(), 1 ether);
+        uint256 mintAmount = (totalCollateralFromAllTokens * numTokensPerEther) / 1e18;
+
+        tf.publishToUniswap(address(ta));
+
+        tokenPool = tf.tokensPools(address(ta));
+        assertNotEq(tokenPool, address(0x0), 'Token pool address is 0x0');
+
+        uint128 liquidity = IUniswapV3Pool(tokenPool).liquidity();
+
+        // TODO: check uniswap pool liquidity
+        //assertEq(liquidity, totalCollateralFromAllTokens, 'Liquidity incorrect');
+
+        vm.stopPrank();
+
+        // TODO: check uniswap pool liquidity and price
+
+        // // TODO: have alice to trade some tokens on uniswap
+
+        // // TODO: have bob / charlie / dave to trade token ta on uniswap
     }
 }
